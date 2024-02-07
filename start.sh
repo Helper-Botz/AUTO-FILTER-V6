7398b6692b403740d5b31c966bb7f7e7ee8ce7aa
HelperCode <109080360+Helper-Botz@users.noreply.github.com>
2024-02-07 20:06:28 +0530

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,12 +1,12 @@
 if [ -z $UPSTREAM_REPO ]
 then
   echo "Cloning main Repository"
-  git clone https://github.com/soulpc/Elsa.git /Elsa
+  git clone https://github.com/ Helper-Botz/MINNALMURALI.git /MINNALMURALI
 else
   echo "Cloning Custom Repo from $UPSTREAM_REPO "
-  git clone $UPSTREAM_REPO /Elsa
+  git clone $UPSTREAM_REPO /MINNALMURALI
 fi
-cd /Elsa
+cd /MINNALMURALI
 pip3 install -U -r requirements.txt
 echo "𝙎𝙩𝙖𝙧𝙩𝙞𝙣𝙜 𝙀𝙡𝙨𝙖....🧞‍♂️"
 python3 bot.py
